6c0289ab2558e7e7101b40180cb6dae651d20826
Jacob Peltola <jtpup0@aim.com>
2015-01-21 12:13:21 -0500

    Camera Dampening and Networking
    -Dampening to camera, figuring out offset part
    -Server Initializes

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject PlayerToFollow;
-    Vector3 cameraOffset = new Vector3(0, 15, 0);
+    public float dampTime = 0.15f;
+    private Vector3 cameraOffset = new Vector3(0, 15, 0);
+    private Vector3 velocity = Vector3.zero;
+    public Transform playerToFollow;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position = PlayerToFollow.transform.position + cameraOffset;
+        //gameObject.transform.position = playerToFollow.transform.position + cameraOffset;
+
+        if (playerToFollow)
+        {
+            Vector3 point = camera.WorldToViewportPoint(playerToFollow.position) + cameraOffset;
+            Vector3 delta = playerToFollow.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
 	
 	}
 }
--- Assets/Scripts/Networking/NetworkManager.js
+++ Assets/Scripts/Networking/NetworkManager.js
@@ -19,7 +19,8 @@ private var btnH : float;
 private var DrawBox : boolean;
 
 //show screen
-function Start () {
+function Start () 
+{
     DrawBox = false;
     btnX = Screen.width * 0.2;
     btnY = Screen.height * 0.05;
@@ -27,52 +28,46 @@ function Start () {
     btnH = Screen.width * 0.1;
 }
 
-function startServer(){
+function startServer()
+{
     Network.InitializeServer(5, 2501, !Network.HavePublicAddress);
     MasterServer.RegisterHost(gameName, "Join Game: Doodle Detective", "Mystery Murder Party Game");
     
 }
 
-function refreshHostLists(){
+function refreshHostLists()
+{
     MasterServer.RequestHostList(gameName);
     refreshing = true;
     Debug.Log(MasterServer.PollHostList().Length);
 }
 
-function Update(){
-    if(refreshing){
-        if(MasterServer.PollHostList().Length > 0){
+function Update()
+{
+    if(refreshing)
+    {
+        if(MasterServer.PollHostList().Length > 0)
+        {
             refreshing = false;
             Debug.Log(MasterServer.PollHostList().Length);
             hostData = MasterServer.PollHostList();
         }
-        //if (networkView.isMine)
-        //{
-        //    InputMovement();
-        //}
-        //else
-        //{
-        //    SyncedMovement();
-        //}
     }
 }
 
-//private function SyncedMovement()
-//{
-//    syncTime += Time.deltaTime;
-//    rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
-//}
-
-function spawnPlayer(){
+function spawnPlayer()
+{
 }
 
 //Messages
-function OnServerInitialized(){
+function OnServerInitialized()
+{
     Debug.Log("Server Initialized!");
     spawnPlayer();
 }
 
-function OnConnectedToServer(){
+function OnConnectedToServer()
+{
     spawnPlayer();
 }
 
@@ -83,38 +78,46 @@ function OnMasterServerEvent(mse:MasterServerEvent)
             Debug.Log("Registered Server!");
         }
     }
-    //GUI
-    function OnGUI(){
-        if (!Network.isClient && !Network.isServer){
 
-            if(GUI.Button(Rect(20, 50, 200, 40), "Host Game")){
+    function OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+
+            if (GUI.Button(Rect(20, 50, 200, 40), "Host Game"))
+            {
                 Debug.Log("Starting Server");
                 startServer();
-                
+        
                 DrawBox = true;
             }
-            if(GUI.Button(Rect(20, 100, 200, 40), "Refresh Hosts")){
+            if (GUI.Button(Rect(20, 100, 200, 40), "Refresh Hosts"))
+            {
                 Debug.Log("Refreshing");
-                
                 refreshHostLists();
             }
-            if(hostData){
-                for(var i:int = 0; i < hostData.length; i++){
-                    if(GUI.Button(Rect(btnX * 1.5 + btnW, btnY * 1.2 + (btnH * i), btnW * 3, btnH * .5), hostData[i].gameName){
-                    Network.Connect(hostData[i]);
-                    
-                    DrawBox = true;
+        
+            if(hostData)
+            {
+                for (var i:int = 0; i < hostData.length; i++)
+                {
+                    if (GUI.Button(Rect(btnX * 1.5 + btnW, btnY * 1.2 + (btnH * i), btnW * 3, btnH * .5), hostData[i].gameName))
+                    {
+                            Network.Connect(hostData[i]);
+                            DrawBox = true;
+                    }
                 }
+            }
         }
-    }	
-}
-if (DrawBox){
-    if(GUI.Button(Rect(140, 50, 100, 40), "Spawn")){
-        Destroy(GameObject.FindWithTag("MainCamera"));
-        Network.Instantiate(player, spawnPoint.position, Quaternion.identity, 0);
-        DrawBox = false;
+    if (DrawBox)
+    {
+        if (GUI.Button(Rect(140, 50, 100, 40), "Spawn"))
+        {
+            Destroy(GameObject.FindWithTag("MainCamera"));
+            Network.Instantiate(player, spawnPoint.position, Quaternion.identity, 0);
+            DrawBox = false;
+        }
     }
-}
 }
 
     
