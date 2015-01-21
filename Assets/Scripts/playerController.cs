d126fbb0de5c6f54610e89088d3c646a98ff8f51
juste119 <mike@playnimbus.com>
2015-01-21 01:11:06 -0500

    Merged playercontroller into demoscene and added analog touch screen controller as well as a simple melee attack

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,6 +6,10 @@ public class playerController : MonoBehaviour {
     public GameObject playerCursor;
     public float playerSpeed;
 
+    public GameObject meleeGO;
+    float meleeTimer = 0;
+    float meleeUpTime = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +17,42 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        MovementAnalog();
 
+        if (meleeGO.active == true)
+        {
+            meleeTimer += Time.deltaTime;
+            if (meleeTimer >= meleeUpTime)
+            {
+                meleeGO.SetActive(false);
+                meleeTimer = 0;
+            }
+        }
 	}
+
+    public void SwingSword()
+    {
+        meleeGO.SetActive(true);
+    }
+
+    public void MovementAnalog()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.name == "AnalogStick")
+                {
+                    Vector3 normalizedCastPosition = hit.point - hit.transform.position;
+                    Vector3 forceToAdd = new Vector3(((hit.point.x - hit.transform.position.x) * playerSpeed), 0, ((hit.point.z - hit.transform.position.z) * playerSpeed));
+                    gameObject.rigidbody.AddForce(forceToAdd);
+                    transform.LookAt(gameObject.transform.position + forceToAdd);
+                }
+            }
+        }
+    }
 
     public void Movement(int direction)
     {
@@ -27,47 +65,47 @@ public class playerController : MonoBehaviour {
         //up
         if (direction == 1)
         {
-            rigidbody2D.velocity = transform.up * playerSpeed;
+            rigidbody.velocity = transform.forward * playerSpeed;
         }
         //upright
         else if(direction == 2)
         {
-            rigidbody2D.velocity = (transform.up + transform.right) * playerSpeed;
+            rigidbody.velocity = (transform.forward + transform.right) * playerSpeed;
         }
         //right
         else if (direction == 3)
         {
-            rigidbody2D.velocity = transform.right * playerSpeed;
+            rigidbody.velocity = transform.right * playerSpeed;
         }
         //downright
         else if (direction == 4)
         {
-            rigidbody2D.velocity = (-transform.up + transform.right) * playerSpeed;
+            rigidbody.velocity = (-transform.forward + transform.right) * playerSpeed;
         }
         //down
         else if (direction == 5)
         {
-            rigidbody2D.velocity = -transform.up * playerSpeed;
+            rigidbody.velocity = -transform.forward * playerSpeed;
         }
         //downleft
         else if (direction == 6)
         {
-            rigidbody2D.velocity = (-transform.up + -transform.right) * playerSpeed;
+            rigidbody.velocity = (-transform.forward + -transform.right) * playerSpeed;
         }
         //left
         else if (direction == 7)
         {
-            rigidbody2D.velocity = -transform.right * playerSpeed;
+            rigidbody.velocity = -transform.right * playerSpeed;
         }
         //upleft
         else if (direction == 8)
         {
-            rigidbody2D.velocity = (transform.up + -transform.right) * playerSpeed;
+            rigidbody.velocity = (transform.forward + -transform.right) * playerSpeed;
         }
         //stop
         else if (direction == 0)
         {
-            rigidbody2D.velocity = Vector3.zero;
+            rigidbody.velocity = Vector3.zero;
         }
     }
 }
