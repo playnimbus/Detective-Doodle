d641a85f2a2b6b0d53d9306cd65da7f45f4aa159
Michael Flood <mike@playnimbus.com>
2015-01-24 23:41:40 -0500

    built the framework for roomcovers
    only built it into one room

---
+++ Assets/RoomReveal.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomReveal : MonoBehaviour {
+
+    public GameObject RoomCover;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player0")
+        {
+            RoomCover.SetActive(false);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player0")
+        {
+            RoomCover.SetActive(true);
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Player0")
+        {
+            RoomCover.SetActive(false);
+        }
+    }
+}
