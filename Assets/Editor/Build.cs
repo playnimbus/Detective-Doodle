a11c47f0c1f9c20f8822269887f556a05352381a
Adam <adam@playnimbus.com>
2015-04-01 00:17:14 -0400

    Building works, now working on a setup to be able to toggle settings from a resource object,

---
+++ Assets/Editor/Build.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Build
+{
+    static string buildFolder = "Builds/";
+    static string sceneFolder = "Assets/Scenes/";
+
+    static string[] masterScenes = { "MasterLobby.unity" };
+    static string[] clientScenes = { "ClientLobby.unity" };
+    static string[] sharedScenes = { "SessionMike.unity" };
+
+    [MenuItem("Build / Create Network Settings")]
+    private static void CreateNetworkSettings()
+    {
+        var obj = ScriptableObject.CreateInstance<NetworkSettings>();
+        string path = AssetDatabase.GenerateUniqueAssetPath("Assets/NetworkSettings");
+        AssetDatabase.CreateAsset(obj, path);
+
+        AssetDatabase.SaveAssets();
+    }
+
+    [MenuItem("Build / Master PC")]
+    private static void BuildMasterPC()
+    {
+        string[] scenes = CombineScenesAndAddFolder(masterScenes, sharedScenes);
+
+        BuildPipeline.BuildPlayer(scenes, buildFolder + "PCMaster/PrivateEyes.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    [MenuItem("Build / Client PC")]
+    private static void BuildClientPC()
+    {
+        string[] scenes = CombineScenesAndAddFolder(clientScenes, sharedScenes);
+
+        BuildPipeline.BuildPlayer(scenes, buildFolder + "PCClient/PrivateEyes.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    [MenuItem("Build / Client Android")]
+    private static void BuildClientAndroid()
+    {
+        string[] scenes = CombineScenesAndAddFolder(clientScenes, sharedScenes);
+
+        BuildPipeline.BuildPlayer(scenes, buildFolder + "AndroidClient/PrivateEyes.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+    [MenuItem("Build / Client iOS")]
+    private static void BuildClientiOS()
+    {
+        string[] scenes = CombineScenesAndAddFolder(clientScenes, sharedScenes);
+
+        BuildPipeline.BuildPlayer(scenes, buildFolder + "iOSClient/PrivateEyes.ipa", BuildTarget.iOS, BuildOptions.None);
+    }
+
+    private static string[] CombineScenesAndAddFolder(string[] groupOne, string[] groupTwo)
+    {
+        int total = groupOne.Length + groupTwo.Length;
+        string[] scenes = new string[total];
+
+        for (int i = 0; i < total; i++)
+        {
+            if (i < groupOne.Length)
+                scenes[i] = sceneFolder + groupOne[i];
+            else
+                scenes[i] = sceneFolder + groupTwo[i - groupOne.Length];
+        }
+
+        return scenes;
+    }
+}
---
+++ Assets/Scripts/AnimateThis.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateThis : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator.SetInteger("pose", 1);
+    }
+}
